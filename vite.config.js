5f7f00cf47d6de09725ded0b261c1b21b6058b2d
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-05-01 18:43:45 +0900

    260501 18:43

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+	base: '/wedding-program/',
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ['react','react-dom'],
+				},
+			},
+		},
+	},
 })
